15c3b00068da7b2202112c368343c9e1324df848
Paul Kim <khyunwoo1@gmail.com>
2024-07-24 11:07:56 -0700

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
+import { Header } from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="app-container">
+        <Header />
         <h3>hi</h3>
       </div>
     </>
