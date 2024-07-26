2447c334a17ae8bb5a0f00b1ceac4729e786a481
Paul Kim <khyunwoo1@gmail.com>
2024-07-25 19:54:16 -0700

    converted names and ip pools into normal variables

--- src/components/NewCard.js
+++ src/components/NewCard.js
@@ -1,35 +1,35 @@
 import React, { useState } from "react";
 import { Button, Grid, Select, MenuItem, InputLabel } from "@material-ui/core";
 
+const names = [
+  "Hye-Jin",
+  "Alexandra",
+  "Noburu",
+  "Paskal",
+  "Itzamna",
+  "Xhafer",
+  "Slobodanka",
+  "Fausta",
+  "Kariuki",
+  "Miriam",
+];
+const ipPools = [
+  "e932fa536fa05c1abe3855389eee39c23c72d813b085bf17c2296ba266f72224b7630fccb1a3c057",
+  "e88adaf0526da97746aee6fdf70884d685b84c6ae88adaf0526da97746aee6fdf70884d685b84c6a",
+  "5a2c74a534f2260efc2229595d09d65f38922d325a2c74a534f2260efc2229595d09d65f38922d32",
+  "87b83b3142f0cc46616f748cf9653e473c00ecda87b83b3142f0cc46616f748cf9653e473c00ecda",
+  "b2c4ee5de82866db38f79c6d4a91a626486b70e9b2c4ee5de82866db38f79c6d4a91a626486b70e9",
+  "8961f68dcbb4af171c24541f30b0d181b7ed77f58961f68dcbb4af171c24541f30b0d181b7ed77f5",
+  "26457d44644a142925c19da2df0c433b3341782826457d44644a142925c19da2df0c433b33417828",
+  "70c881d4a26984ddce795f6f71817c9cf4480e7970c881d4a26984ddce795f6f71817c9cf4480e79",
+  "da23614e02469a0d7c7bd1bdab5c9c474b1904dcda23614e02469a0d7c7bd1bdab5c9c474b1904dc",
+  "a9993e364706816aba3e25717850c26c9cd0d89da9993e364706816aba3e25717850c26c9cd0d89d",
+];
 export const NewCard = ({
   newZoneObj,
   cancelNewNetworkSecurityInfo,
   addNewNetworkSecurityZones,
 }) => {
-  const [names, setNames] = useState([
-    "Hye-Jin",
-    "Alexandra",
-    "Noburu",
-    "Paskal",
-    "Itzamna",
-    "Xhafer",
-    "Slobodanka",
-    "Fausta",
-    "Kariuki",
-    "Miriam",
-  ]);
-  const [ipPools, setIPPools] = useState([
-    "e932fa536fa05c1abe3855389eee39c23c72d813b085bf17c2296ba266f72224b7630fccb1a3c057",
-    "e88adaf0526da97746aee6fdf70884d685b84c6ae88adaf0526da97746aee6fdf70884d685b84c6a",
-    "5a2c74a534f2260efc2229595d09d65f38922d325a2c74a534f2260efc2229595d09d65f38922d32",
-    "87b83b3142f0cc46616f748cf9653e473c00ecda87b83b3142f0cc46616f748cf9653e473c00ecda",
-    "b2c4ee5de82866db38f79c6d4a91a626486b70e9b2c4ee5de82866db38f79c6d4a91a626486b70e9",
-    "8961f68dcbb4af171c24541f30b0d181b7ed77f58961f68dcbb4af171c24541f30b0d181b7ed77f5",
-    "26457d44644a142925c19da2df0c433b3341782826457d44644a142925c19da2df0c433b33417828",
-    "70c881d4a26984ddce795f6f71817c9cf4480e7970c881d4a26984ddce795f6f71817c9cf4480e79",
-    "da23614e02469a0d7c7bd1bdab5c9c474b1904dcda23614e02469a0d7c7bd1bdab5c9c474b1904dc",
-    "a9993e364706816aba3e25717850c26c9cd0d89da9993e364706816aba3e25717850c26c9cd0d89d",
-  ]);
   const [selectedName, setSelectedName] = useState(names[0]);
   const [selectedIPPool, setSelectedIPPool] = useState(ipPools[0]);
   const handleNameChange = (event) => {
